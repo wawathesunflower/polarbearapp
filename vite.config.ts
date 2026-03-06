ec2f35ab8a95517a817f2a06fb9e13984d881a9a
Eunice Wachera <info@eternityin-amoment.org>
2026-03-06 12:55:38 +0300

    Fixed white screen via vite.config.ts and verified paths.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/polarbearapp/', // This tells Vite to look in the right folder
+  base: './', // This tells Vite to look in the right folder
   plugins: [react()],
 })
