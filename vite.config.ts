605bc159bdd2db6caace5cc7d62affbc30233ffb
Eunice Wachera <info@eternityin-amoment.org>
2026-03-06 12:34:41 +0300

    Fix path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/polarbearapp/', // This tells Vite to look in the right folder
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
